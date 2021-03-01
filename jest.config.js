f16e4c28bdac6fbb6ed8983dcc01d9eb6c9cc046
liujun04 <liujun04@aikucun.com>
2021-03-01 15:46:37 +0800

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 const jestConfig = require('kcd-scripts/jest')
-jestConfig.roots = [
-  'packages/ios/src',
-  'packages/android/src',
-  'packages/core/src',
-]
+jestConfig.roots = ['packages/ios/src', 'packages/core/src']
 jestConfig.collectCoverageFrom = ['packages/**/*.{js,jsx,ts,tsx}']
 jestConfig.testEnvironment = 'jsdom'
 module.exports = jestConfig
